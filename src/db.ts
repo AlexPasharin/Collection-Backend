d471e26c57a98379c99666bddb9f496438d36597
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2019-10-11 09:35:44 +0300

    refactoring of rest endpoints

--- src/db.ts
+++ src/db.ts
@@ -16,6 +16,8 @@ const psConfig = ({
 //   }
 // }
 
+export type dbQueryFunc = () => Promise<any[]>
+
 export class dbConnection {
   dbInstance: any;
 
@@ -35,98 +37,111 @@ export class dbConnection {
       });
   }
 
-  getArtists = () => this.dbInstance('artists').select()
+  getArtists: () => dbQueryFunc =
+    () => () => this.dbInstance('artists').select()
 
-  getArtistTypes = (artistID: number) => this.dbInstance
-    .distinct('t.id', 't.name')
-    .select()
-    .from('types as t')
-    .leftJoin(
-      'entries as e',
-      'e.type',
-      't.id',
-    )
-    .where('artist_id', artistID)
-    .orWhere('entry_artist_id', artistID)
-
-  getEntriesByArtistAndType = (artistID: number, typeID: number) =>
-    this.dbInstance
+  getArtistTypes: (artistID: number) => dbQueryFunc =
+    artistID => () => this.dbInstance
+      .distinct('t.id', 't.name')
       .select()
-      .from('entries')
-      .where({
-        artist_id: artistID,
-        type: typeID,
-      })
-      .orWhere({
-        entry_artist_id: artistID,
-        type: typeID,
-      })
-
-  getReleases = (entry: number) =>
-    this.dbInstance
-      .select()
-      .from('releases')
-      .where({
-        entry_id: entry
-      })
-
-  getRelease = (id: Number) =>
-    this.dbInstance
-      .table('releases')
-      .first()
-      .where({
-        id
-      })
-
-  getEntry = (id: Number) =>
-    this.dbInstance
-      .table('entries')
-      .first()
-      .where({
-        id
-      })
-
-  getLabels = () =>
-    this.dbInstance('labels')
-      .select()
-
-  getFormats = () =>
-    this.dbInstance('formats')
-      .select()
-
-  getCountries = () =>
-    this.dbInstance('countries')
-      .select()
-
-  addRelease = release =>
-    this.dbInstance('releases')
-      .returning('id')
-      .insert(release)
-      .then(([release_id]) => ({ release_id }))
-
-  updateRelease = release =>
-    this.dbInstance('releases')
-      .where({ id: release.id })
-      .update(release)
-      .then(() => this.getRelease(release.id))
-
-  getReleaseTracks = release_id =>
-    this.dbInstance('compositions as c')
-      .join(
-        'tracks as t',
-        'c.id',
-        't.composition_id',
+      .from('types as t')
+      .leftJoin(
+        'entries as e',
+        'e.type',
+        't.id',
       )
-      .join(
-        'release_tracks as rt',
-        'rt.track_id',
-        't.id'
-      )
-      .select(['name', 't.id', 'composition_id', 'alt_name', 'version', 'performer_id', 'release_id', 'track_id', 'number', 'subversion', 'comment', 'length', 'place'])
-      .where({ release_id })
+      .where('artist_id', artistID)
+      .orWhere('entry_artist_id', artistID)
 
-  getCompositions = () =>
-    this.dbInstance('compositions').select()
+  getEntriesByArtistAndType: (artistID: number, typeID: number) => dbQueryFunc =
+    (artistID, typeID) => () =>
+      this.dbInstance
+        .select()
+        .from('entries')
+        .where({
+          artist_id: artistID,
+          type: typeID,
+        })
+        .orWhere({
+          entry_artist_id: artistID,
+          type: typeID,
+        })
+
+  getReleases: (entry: number) => dbQueryFunc =
+    entry => () =>
+      this.dbInstance
+        .select()
+        .from('releases')
+        .where({
+          entry_id: entry
+        })
+
+  getRelease: (id: Number) => dbQueryFunc =
+    id => () =>
+      this.dbInstance
+        .table('releases')
+        .first()
+        .where({
+          id
+        })
+
+  getEntry: (id: Number) => dbQueryFunc =
+    id => () =>
+      this.dbInstance
+        .table('entries')
+        .first()
+        .where({
+          id
+        })
+
+  getLabels: () => dbQueryFunc =
+    () => () =>
+      this.dbInstance('labels')
+        .select()
+
+  getFormats: () => dbQueryFunc =
+    () => () =>
+      this.dbInstance('formats')
+        .select()
+
+  getCountries: () => dbQueryFunc =
+    () => () =>
+      this.dbInstance('countries')
+        .select()
+
+  addRelease: (release: any) => dbQueryFunc =
+    release => () =>
+      this.dbInstance('releases')
+        .returning('id')
+        .insert(release)
+        .then(([release_id]) => ({ release_id }))
+
+  updateRelease: (release: any) => dbQueryFunc =
+    release => () =>
+      this.dbInstance('releases')
+        .where({ id: release.id })
+        .update(release)
+        .then(() => this.getRelease(release.id))
+
+  getReleaseTracks: (release_id: any) => dbQueryFunc =
+    release_id => () =>
+      this.dbInstance('compositions as c')
+        .join(
+          'tracks as t',
+          'c.id',
+          't.composition_id',
+        )
+        .join(
+          'release_tracks as rt',
+          'rt.track_id',
+          't.id'
+        )
+        .select(['name', 't.id', 'composition_id', 'alt_name', 'version', 'performer_id', 'release_id', 'track_id', 'number', 'subversion', 'comment', 'length', 'place'])
+        .where({ release_id })
+
+  getCompositions: () => dbQueryFunc =
+    () => () =>
+      this.dbInstance('compositions').select()
 }
 
 export default (new dbConnection)
--- src/server/restEndpoints.ts
+++ src/server/restEndpoints.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express'
 import * as cors from 'cors'
 
-import { dbConnection } from "../db"
+import { dbConnection, dbQueryFunc } from "../db"
 
 const successHandler = res => data => res.set("Access-Control-Allow-Origin", "*").json(data)
 
@@ -10,98 +10,129 @@ const errorHandler = (res, message: string) => err => {
   res.set("Access-Control-Allow-Origin", "*").status(500).send(message)
 }
 
+const handler = (promiseFunc: dbQueryFunc, res, errorMsn: string) => {
+  promiseFunc()
+    .then(successHandler(res))
+    .catch(errorHandler(res, errorMsn))
+}
+
 export const setUpRestEndPoints = (app: Express, dBConnection: dbConnection) => {
   app.get('/rest/artists', (_, res) =>
-    dBConnection
-      .getArtists()
-      .then(successHandler(res))
-      .catch(errorHandler(res, 'Could not retrieve artists from the database'))
+    handler(
+      dBConnection.getArtists(),
+      res,
+      'Could not retrieve artists from the database'
+    )
   )
 
   app.get('/rest/types', (req, res) => {
     const { artist } = req.query
 
-    dBConnection
-      .getArtistTypes(Number(artist))
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve artist\'s with id ${artist} entry types from the database`))
+    handler(
+      dBConnection.getArtistTypes(Number(artist)),
+      res,
+      `Could not retrieve entry types of artist\'s with id ${artist} from the database`
+    )
   })
 
   app.get('/rest/entries', (req, res) => {
     const { artist, type } = req.query
 
-    dBConnection.getEntriesByArtistAndType(Number(artist), Number(type))
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve artist\'s ${artist} entries of type ${type} from the database`))
+    handler(
+      dBConnection.getEntriesByArtistAndType(Number(artist), Number(type)),
+      res,
+      `Could not retrieve artist\'s ${artist} entries of type ${type} from the database`
+    )
   })
 
   app.get('/rest/entry', (req, res) => {
     const { id } = req.query
 
-    dBConnection.getEntry(id)
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve entry ${id} from the database`))
+    handler(
+      dBConnection.getEntry(id),
+      res,
+      `Could not retrieve entry ${id} from the database`
+    )
   })
 
   app.get('/rest/releases', (req, res) => {
     const { entry } = req.query
 
-    dBConnection.getReleases(Number(entry))
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve releases of an entry ${entry} from the database`))
+    handler(
+      dBConnection.getReleases(Number(entry)),
+      res,
+      `Could not retrieve releases of an entry ${entry} from the database`
+    )
   })
 
   app.get('/rest/release', (req, res) => {
     const { id } = req.query
 
-    dBConnection.getRelease(Number(id))
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve release with id ${id} from the database`))
+    handler(
+      dBConnection.getRelease(Number(id)),
+      res,
+      `Could not retrieve release with id ${id} from the database`
+    )
   })
 
   app.get('/rest/labels', (_, res) => {
-    dBConnection.getLabels()
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve labels from the database`))
+
+    handler(
+      dBConnection.getLabels(),
+      res,
+      `Could not retrieve labels from the database`
+    )
   })
 
   app.get('/rest/formats', (_, res) => {
-    dBConnection.getFormats()
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve formats from the database`))
+    handler(
+      dBConnection.getFormats(),
+      res,
+      `Could not retrieve formats from the database`
+    )
   })
 
   app.get('/rest/countries', (_, res) => {
-    dBConnection.getCountries()
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve countries from the database`))
+    handler(
+      dBConnection.getCountries(),
+      res,
+      `Could not retrieve countries from the database`
+    )
   })
 
   app.options('/rest/release', cors({ origin: process.env.CORS_ORIGIN_URL }))
+
   app.post('/rest/release', cors({ origin: process.env.CORS_ORIGIN_URL }), (req, res) => {
-    dBConnection.addRelease(req.body)
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not add release to the database`))
+    handler(
+      dBConnection.addRelease(req.body),
+      res,
+      "Could not add release to the database"
+    )
   })
 
   app.put('/rest/release', cors({ origin: process.env.CORS_ORIGIN_URL }), (req, res) => {
-    dBConnection.updateRelease(req.body)
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not add update release`))
+    handler(
+      dBConnection.updateRelease(req.body),
+      res,
+      `Could not update release ${req.body.id}`
+    )
   })
 
   app.get('/rest/tracks', (req, res) => {
     const { release_id } = req.query
-
-    dBConnection.getReleaseTracks(release_id)
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve tracks of release with id ${release_id} from the database`))
+    handler(
+      dBConnection.getReleaseTracks(release_id),
+      res,
+      `Could not retrieve tracks of release with id ${release_id} from the database`
+    )
   })
 
-  app.get('/rest/compositions', (req, res) => {
-    dBConnection.getCompositions()
-      .then(successHandler(res))
-      .catch(errorHandler(res, `Could not retrieve compositions from the database`))
+  app.get('/rest/compositions', (_, res) => {
+    handler(
+      dBConnection.getCompositions(),
+      res,
+      `Could not retrieve compositions from the database`
+    )
   })
 
   return app
