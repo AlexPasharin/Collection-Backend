78d39b4a2c2c1edd415a46f7185ed67aaa06c3f2
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2022-11-07 18:08:23 +0200

    try fix

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,9 @@
+const parse = require("pg-connection-string").parse;
+
 const config = {
   client: "pg",
   connection: process.env.DATABASE_URL
-    ? `${process.env.DATABASE_URL}`
+    ? getConnection()
     : {
         host: "localhost",
         //  database: 'aleksandrpasharin',
@@ -10,3 +12,11 @@ const config = {
 };
 
 module.exports = config;
+
+function getConnection() {
+  const connection = parse(`${process.env.DATABASE_URL}?ssl=true`);
+
+  connection.ssl = { rejectUnauthorized: false };
+
+  return connection;
+}
